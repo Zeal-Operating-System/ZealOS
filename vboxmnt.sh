386132de0291ca46fa5814cb8e7266ae83c13d54
xmm15 <voidnv@protonmail.com>
2020-02-15 14:31:41 -0600

    Made src directory

--- vboxmnt.sh
+++ vboxmnt.sh
@@ -2,7 +2,10 @@
 sudo modprobe nbd
 sudo qemu-nbd -c dev/nbd0 ~/VirtualBox\ VMs/ZenithOS/ZenithOS.qcow
 sudo mount /dev/nbd0p1 /mnt
-sudo cp -r /mnt/* ~/Projects/ZenithOS/
+
+rm -rf src/*
+sudo cp -r /mnt/* src/
+
 sudo umount /mnt
 sudo qemu-nbd -d /dev/nbd0
-sudo chown -R v:v ~/Projects/ZenithOS/*
+sudo chown -R v:v src/*
