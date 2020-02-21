11b826c61f0ce333b3b76ccc024ff44ad183426c
xmm15 <voidnv@protonmail.com>
2020-02-21 14:25:33 -0600

    rm Zenith-latest

--- vboxmnt.sh
+++ vboxmnt.sh
@@ -9,4 +9,5 @@ sudo cp -r /mnt/* src/
 sudo umount /mnt
 sudo qemu-nbd -d /dev/nbd0
 sudo chown -R v:v src/*
+rm Zenith-latest*
 mv src/Tmp/MyDistro.ISO.C ./Zenith-latest-$(date +%Y-%m-%d-%H_%M_%S).iso
