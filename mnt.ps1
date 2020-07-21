1b8d1223d672d477f6b1fdf97ce793a57c6b4a86
VoidNV <voidnv@protonmail.com>
2020-07-21 15:54:17 -0500

    Rename mnt.ps1 to export.ps1

--- mnt.ps1
+++
@@ -1,6 +0,0 @@
-$dir = "~\Projects\ZenithOS"
-Remove-Item -Recurse -Force ${dir}\src\*
-Copy-Item -Recurse -Force Z:/* ${dir}\src\
-Remove-Item ${dir}\*.iso
-Move-Item ${dir}\src\Tmp\MyDistro.ISO.C ${dir}\Zenith-latest-$(Get-Date -Format "yyyy-MM-dd-HH_mm_ss").iso
-
