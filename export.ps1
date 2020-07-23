d7278966c8464292f82aaa88c49cf1c16381f65f
VoidNV <voidnv@protonmail.com>
2020-07-23 17:20:48 -0500

    Refactor export.ps1

--- export.ps1
+++ export.ps1
@@ -1,6 +1,4 @@
-$dir = "~\Projects\ZenithOS"
-Remove-Item -Recurse -Force ${dir}\src\*
-Copy-Item -Recurse -Force Z:/* ${dir}\src\
-Remove-Item ${dir}\*.iso
-Move-Item ${dir}\src\Tmp\MyDistro.ISO.C ${dir}\Zenith-latest-$(Get-Date -Format "yyyy-MM-dd-HH_mm_ss").iso
-
+Remove-Item -Recurse -Force .\src\*
+Copy-Item -Recurse -Force Z:\* .\src\
+Remove-Item .\Zenith-latest*.iso
+Move-Item .\src\Tmp\MyDistro.ISO.C .\Zenith-latest-$(Get-Date -Format "yyyy-MM-dd-HH_mm_ss").iso
