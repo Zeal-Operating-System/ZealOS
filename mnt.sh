01e13ff62dee6108fa859b5ee6f1ec32b5932029
TomAwezome <sonidd1@yahoo.com>
2021-07-03 02:47:45 -0400

    Fix codebase HTML generation.

--- mnt.sh
+++ mnt.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "mounting..."
 sudo modprobe nbd
-sudo qemu-nbd -c dev/nbd0 ~/VirtualBox\ VMs/ZealOS/ZealOS.vdi #Replace with path to disk
+#sudo qemu-nbd -c dev/nbd0 ~/VirtualBox\ VMs/ZealOS/ZealOS.vdi #Replace with path to disk
+sudo qemu-nbd -c dev/nbd0 ~/vmware/ZAHCI/ZAHCI.vmdk #Replace with path to disk
 sudo partprobe /dev/nbd0
 sudo mount /dev/nbd0p1 /mnt
 echo "mounted .vdi"
@@ -10,7 +11,13 @@ echo "removing src/ files"
 rm -rf src/*
 echo "copying src/ files from root of mounted .vdi"
 sudo cp -r /mnt/* src/
-echo "files copied"
+echo "src/ files copied"
+
+echo "removing doc/ files"
+rm -rf docs/*
+echo "copying docs/ files from HTML/ folder of mounted .vdi"
+sudo cp -r /mnt/HTML/* docs/
+echo "docs/ files copied"
 
 echo "unmounting..."
 sudo umount /mnt
@@ -18,7 +25,12 @@ sudo qemu-nbd -d /dev/nbd0
 echo "unmounted .vdi"
 echo "set perms, update ISO."
 sudo chown -R $USER:$USER src/*
+sudo chown -R $USER:$USER docs/*
 rm ZealOS-*
 mv src/Tmp/MyDistro.ISO.C ./ZealOS-$(date +%Y-%m-%d-%H_%M_%S).iso
+
+echo "removing duplicates"
+rm -rf ./src/HTML/*
+
 echo "finished."
 git status
