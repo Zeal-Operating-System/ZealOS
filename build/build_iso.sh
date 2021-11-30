c20721e7a4733b4620225cedbff59effa42853c8
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-11-29 19:54:36 -0700

    transfer

--- build/build_iso.sh
+++ build/build_iso.sh
@@ -8,38 +8,39 @@ echo "Making temp HDD, running auto-install"
 qemu-img create -f raw ZealOS.raw 192M
 qemu-system-x86_64 -machine q35,kernel_irqchip=off,accel=kvm -drive format=raw,file=ZealOS.raw -m 2G -smp $(nproc) -rtc base=localtime -cdrom AUTO.ISO -device isa-debug-exit
 echo "Mounting"
-sudo rm /mnt/* -r
+sudo mkdir /tmp/zealtmp
+sudo rm /tmp/zealtmp/* -r
 sudo sync
 sudo modprobe nbd
 sudo qemu-nbd -c dev/nbd0 -f raw ./ZealOS.raw
 sudo partprobe /dev/nbd0
 echo "Merging with src/"
-sudo mount /dev/nbd0p1 /mnt
+sudo mount /dev/nbd0p1 /tmp/zealtmp
 rm ../src/Home/Registry.CC # we use Registry for auto-iso process, don't want to overwrite
 rm ../src/Home/MakeHome.CC # unneeded file
-sudo cp -r ../src/* /mnt/
+sudo cp -r ../src/* /tmp/zealtmp/
 sudo sync
 echo "Files copied, unmounting"
-sudo umount /mnt
+sudo umount /tmp/zealtmp
 sudo qemu-nbd -d /dev/nbd0
-sudo rm /mnt/* -r
+sudo rm /tmp/zealtmp/* -r
 sudo sync
 qemu-system-x86_64 -machine q35,kernel_irqchip=off,accel=kvm -drive format=raw,file=ZealOS.raw -m 2G -smp $(nproc) -rtc base=localtime -device isa-debug-exit
 echo "Mounting"
-sudo rm /mnt/* -r
+sudo rm /tmp/zealtmp/* -r
 sudo sync
 sudo modprobe nbd
 sudo qemu-nbd -c dev/nbd0 -f raw ./ZealOS.raw
 sudo partprobe /dev/nbd0
-sudo mount /dev/nbd0p1 /mnt
+sudo mount /dev/nbd0p1 /tmp/zealtmp
 echo "Extracting Distro ISO"
 sudo rm ./ZealOS-*.iso # comment this line if you want lingering old ISOs
-sudo cp /mnt/Tmp/MyDistro.ISO.C ./ZealOS-$(date +%Y-%m-%d-%H_%M_%S).iso
+sudo cp /tmp/zealtmp/Tmp/MyDistro.ISO.C ./ZealOS-$(date +%Y-%m-%d-%H_%M_%S).iso
 sudo chown -R $USER:$USER ./ZealOS-*.iso
 echo "Files copied, unmounting"
-sudo umount /mnt
+sudo umount /tmp/zealtmp
 sudo qemu-nbd -d /dev/nbd0
-sudo rm /mnt/* -r
+sudo rm /tmp/zealtmp/* -r
 sudo sync
 echo "Deleting temp HDD"
 rm ./ZealOS.raw
