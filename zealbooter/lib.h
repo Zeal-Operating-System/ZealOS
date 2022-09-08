eac6284cea30d02062c859f8c2cc5df2a8d324bb
TomAwezome <sonidd1@yahoo.com>
2022-09-08 17:46:29 -0400

    Add #include guards to zealbooter library files using ifndef and endif.

--- zealbooter/lib.h
+++ zealbooter/lib.h
@@ -1,3 +1,6 @@
+#ifndef __LIB_H__
+#define __LIB_H__
+
 #include <stdint.h>
 #include <memcpy.h>
 #include <print.h>
@@ -6,3 +9,5 @@ uint64_t div_roundup_u64(uint64_t a, uint64_t b);
 uint64_t align_up_u64(uint64_t a, uint64_t b);
 
 typedef char symbol[];
+
+#endif // __LIB_H__
